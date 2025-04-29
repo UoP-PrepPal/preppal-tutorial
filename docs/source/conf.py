689ea262902d941c08c113443a787ca4f404ea48
Jonathan Parker <jonathanjparker2@gmail.com>
2025-04-29 10:51:13 +0100

    updated project information

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PrepPal'
+copyright = ''
+author = 'Team 4D'
 
 release = '0.1'
 version = '0.1.0'
